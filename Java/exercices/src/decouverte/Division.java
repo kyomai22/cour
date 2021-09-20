ab33041009efdb5b818e10d5c8f8e006cfc91652
Dimitri Schira <39882119+kyomai22@users.noreply.github.com>
2021-09-20 13:26:49 +0200

    Ajout Java

---
+++ Java/exercices/src/decouverte/Division.java
@@ -0,0 +1,34 @@
+package decouverte;
+
+import java.util.Scanner;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int nb1, nb2, nb3;
+		//
+		System.out.println("Bonjour, entrez une valeure à divisé");
+		nb1=(new Scanner(System.in)).nextInt();
+		
+		System.out.println("Entrez le diviseur");
+		nb2=(new Scanner(System.in)).nextInt();
+		
+		System.out.println("Quel est la réponse?");
+		nb3=(new Scanner(System.in)).nextInt();
+		
+		if(nb2==0){
+			System.out.println("0 n'est pas une entrée valide");
+			
+		}
+		else {
+			if (nb1/nb2==nb3){
+				System.out.println("Bravo !");
+			}
+			else{
+				System.out.println("Non la bonne réponse est "+(nb1/nb2));
+			}
+		}
+		
+
+	}
+}
