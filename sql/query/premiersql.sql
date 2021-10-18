73fc03c3a99bb51eb43485a7008fde844718e60c
Dimitri Schira <39882119+kyomai22@users.noreply.github.com>
2021-10-18 14:38:44 +0200

    Update premiersql.sql

--- sql/query/premiersql.sql
+++ sql/query/premiersql.sql
@@ -1,7 +1,9 @@
 use introprog2021c;
 /*
 */
-create table formations(for_code integer primary key auto_increment,
-for_nom varchar(50) not null,
-for_annee year(4) not null check(for_annee>2000),
-unique(for_nom,for_annee));
+CREATE TABLE formations (
+    for_code INTEGER PRIMARY KEY AUTO_INCREMENT,
+    for_nom VARCHAR(50) NOT NULL,
+    for_annee YEAR(4) NOT NULL CHECK (for_annee > 2000),
+    UNIQUE (for_nom , for_annee)
+);
