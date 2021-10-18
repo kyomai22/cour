2b4fc7603e353fe474749699687db9f6b61454fa
Dimitri Schira <39882119+kyomai22@users.noreply.github.com>
2021-10-18 16:05:25 +0200

    Update premiersql.sql

--- sql/query/premiersql.sql
+++ sql/query/premiersql.sql
@@ -22,3 +22,28 @@ values ('java',2021);
 insert into formations (for_nom, for_annee) 
 values ('java',2022);
 update formations set for_nom='Web dev' where for_code = 4;
+delete from formations where for_code=4;
+
+CREATE TABLE stagiaires (
+    sta_code INTEGER PRIMARY KEY AUTO_INCREMENT,
+    sta_nom VARCHAR(30) NOT NULL CHECK (TRIM(sta_nom) <> ''),
+    sta_prenom VARCHAR(30) NOT NULL CHECK (TRIM(sta_prenom) <> ''),
+    sta_ddn DATE,
+    sta_fk_formations INTEGER NULL,
+    INDEX (sta_fk_formations),
+    FOREIGN KEY (sta_fk_formations)
+        REFERENCES formations (for_code)
+);
+insert into stagiaires values (default, 'Kevin', 'Calcul', '1999-05-12',7);
+SELECT 
+    *
+FROM
+    stagiaires;
+    
+    SELECT 
+    *
+FROM
+    stagiaires,
+    formations
+WHERE
+    sta_fk_formations = for_code;
